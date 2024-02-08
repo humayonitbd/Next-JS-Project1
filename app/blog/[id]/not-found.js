70ce8e2e1f3b859c6cef9696d426300d86554d29
humayonitbd <humayonforid44@gmail.com>
2024-02-08 16:29:32 +0600

    dinamic page not foundgit add .

---
+++ app/blog/[id]/not-found.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div>Blogs page is not founds!!</div>
+  )
+}
--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,7 +1,11 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 export default function BlogPage({params}) {
     const {id} = params;
+    if(id === '4'){
+        notFound();
+    }
   return (
     <div>The Blog id is {id}</div>
   )
