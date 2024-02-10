151641afdd56d21aabdfbc8ff8ddf2bbb531fb24
humayonitbd <humayonforid44@gmail.com>
2024-02-10 23:31:16 +0600

    single page design

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -5,9 +5,10 @@ export default async function PostSingle({params}) {
     const {id} = params;
     const post = await getPost(id);
   return (
-    <div>
+    <div className="bg-orange-300 text-black p-2">
         <h2>Single Page Post Id {post.id}</h2>
         <p>{post.title}</p>
+        <p>{post.body}</p>
     </div>
   )
 }
